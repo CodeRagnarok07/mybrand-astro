abf2204bb8eadcf27b06d1e138fc145f2e8c8563
ragnarok <angelriera1796@gmail.com>
2023-09-22 12:50:16 -0400

    add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import { VitePWA } from 'vite-plugin-pwa';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [VitePWA()],
-	},
+  vite: {
+    plugins: [VitePWA()]
+  },
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
