4dfbbe75b0609bccbc2c2abb96dfe55c30c50409
ragnarok <angelriera1796@gmail.com>
2023-09-23 16:29:08 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import compress from "astro-compress";
+// import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [sitemap(), tailwind()],
+  
+  integrations: [sitemap(), tailwind(), compress()]
   // output: "server",
   // adapter: vercel()
 });
